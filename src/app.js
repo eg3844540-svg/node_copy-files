6245e17945e48b391b2d61afac95605e19ed912d
ericagomes-dev <ericagomesdev@gmail.com>
2026-08-09 19:31:17 -0300

    Implement file copy app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const fs = require('fs');
+
+const source = process.argv[2];
+const destination = process.argv[3];
+
+if (!source || !destination) {
+  console.error('Source and destination are required');
+} else if (source !== destination) {
+  try {
+    fs.copyFileSync(source, destination);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
