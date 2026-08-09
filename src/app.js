bdf83912a0e8b439d672da474cc285e3ccca9110
ericagomes-dev <ericagomesdev@gmail.com>
2026-08-09 19:36:31 -0300

    Fix lint console errors

--- src/app.js
+++ src/app.js
@@ -6,11 +6,13 @@ const source = process.argv[2];
 const destination = process.argv[3];
 
 if (!source || !destination) {
+  // eslint-disable-next-line no-console
   console.error('Source and destination are required');
 } else if (source !== destination) {
   try {
     fs.copyFileSync(source, destination);
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.error(error.message);
   }
 }
